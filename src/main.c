e14e131b128882a80d7224d8359deeb06919c234
meequrox <117393211+meequrox@users.noreply.github.com>
2023-03-24 23:35:27 +0700

    Rename variables to be more descriptive

--- src/main.c
+++ src/main.c
@@ -4,24 +4,27 @@
 #include <string.h>
 
 struct MemoryStruct {
-    char* memory;
+    char* data;
     size_t size;
 };
 
-size_t write_memory_callback(void* contents, size_t size, size_t nmemb, void* userp) {
-    size_t realsize = size * nmemb;
-    struct MemoryStruct* mem = (struct MemoryStruct*)userp;
+size_t write_memory_callback(void* contents, size_t size, size_t nmemb, struct MemoryStruct* userdata) {
+    if (!userdata) {
+        fprintf(stderr, "%s: userdata is NULL\n", __FUNCTION__);
+        return 0;
+    }
 
-    char* ptr = realloc(mem->memory, mem->size + realsize + 1);
+    size_t realsize = size * nmemb;
+    char* ptr = realloc(userdata->data, userdata->size + realsize + 1);
     if (!ptr) {
         fprintf(stderr, "%s: not enough memory\n", __FUNCTION__);
         return 0;
     }
 
-    mem->memory = ptr;
-    memcpy(&(mem->memory[mem->size]), contents, realsize);
-    mem->size += realsize;
-    mem->memory[mem->size] = 0;
+    userdata->data = ptr;
+    memcpy(&(userdata->data[userdata->size]), contents, realsize);
+    userdata->size += realsize;
+    userdata->data[userdata->size] = 0;
 
     return realsize;
 }
@@ -32,12 +35,12 @@ int main() {
 
     const char* baseurl = "https://flareboard.ru/thread.php?id=1327";
     //    const char* baseurl = "https://flareboard.ru/thread.php?id=2666";
-    struct MemoryStruct chunk = {malloc(1), 0};
+    struct MemoryStruct memory = {malloc(1), 0};
 
     curl_easy_setopt(curl_handle, CURLOPT_URL, baseurl);
     curl_easy_setopt(curl_handle, CURLOPT_FOLLOWLOCATION, 0);
     curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, write_memory_callback);
-    curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, (void*)&chunk);
+    curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, (void*)&memory);
     curl_easy_setopt(curl_handle, CURLOPT_USERAGENT,
                      "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:99.0) Gecko/20100101 Firefox/99.0");
 
@@ -45,13 +48,13 @@ int main() {
     curl_easy_cleanup(curl_handle);
 
     if (response == CURLE_OK) {
-        fprintf(stdout, "%lu bytes retrieved\n", (unsigned long)chunk.size);
+        fprintf(stdout, "%lu bytes retrieved\n", (unsigned long)memory.size);
     } else {
         fprintf(stderr, "curl_easy_perform() failed: %s", curl_easy_strerror(response));
     }
 
-    fprintf(stdout, "%s\n", chunk.memory);
-    free(chunk.memory);
+    fprintf(stdout, "%s\n", memory.data);
+    free(memory.data);
     curl_global_cleanup();
     return 0;
 }
