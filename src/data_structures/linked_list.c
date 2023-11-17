4bd41453e3c9511b73cafa170ec41df8a6343faa
meequrox <117393211+meequrox@users.noreply.github.com>
2023-11-18 03:02:44 +0700

    data_structures: remove rbtree, improve linked list

--- src/data_structures/linked_list.c
+++ src/data_structures/linked_list.c
@@ -3,8 +3,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-flb_list_node* flb_list_create_node(const char* value) {
-    if (!value) {
+flb_list_node* flb_list_create_node(const char* key, const char* value) {
+    if (!key) {
         return NULL;
     }
 
@@ -14,23 +14,41 @@ flb_list_node* flb_list_create_node(const char* value) {
         return NULL;
     }
 
-    const size_t value_len = strlen(value);
-    char* value_copy = strndup(value, value_len);
+    const size_t key_len = strlen(key);
+    char* key_copy = strndup(key, key_len);
 
-    if (!value_copy) {
+    if (!key_copy) {
         free(node);
         return NULL;
     }
 
+    char* value_copy = NULL;
+    size_t value_len = 0;
+
+    if (value) {
+        value_len = strlen(value);
+        value_copy = strdup(value);
+
+        if (!value_copy) {
+            free(node);
+            free(key_copy);
+            return NULL;
+        }
+    }
+
+    node->key = key_copy;
+    node->key_len = key_len;
+
     node->value = value_copy;
     node->value_len = value_len;
+
     node->next = NULL;
 
     return node;
 }
 
-flb_list_node* flb_list_insert_front(flb_list_node* list, const char* value) {
-    flb_list_node* new_node = flb_list_create_node(value);
+flb_list_node* flb_list_insert_front(flb_list_node* list, const char* key, const char* value) {
+    flb_list_node* new_node = flb_list_create_node(key, value);
 
     if (!new_node) {
         return list;
@@ -40,9 +58,9 @@ flb_list_node* flb_list_insert_front(flb_list_node* list, const char* value) {
     return new_node;
 }
 
-__attribute__((unused)) flb_list_node* flb_list_lookup(flb_list_node* list, const char* value) {
+__attribute__((unused)) flb_list_node* flb_list_lookup(flb_list_node* list, const char* key) {
     while (list) {
-        if (strcmp(list->value, value) == 0) {
+        if (strcmp(list->key, key) == 0) {
             return list;
         }
 
@@ -52,18 +70,18 @@ __attribute__((unused)) flb_list_node* flb_list_lookup(flb_list_node* list, cons
     return NULL;
 }
 
-__attribute__((unused)) flb_list_node* flb_list_delete(flb_list_node* list, const char* value) {
+__attribute__((unused)) flb_list_node* flb_list_delete(flb_list_node* list, const char* key) {
     for (flb_list_node *prev = NULL, *cur = list; cur; cur = cur->next) {
-        if (strcmp(cur->value, value) == 0) {
+        if (strcmp(cur->key, key) == 0) {
             if (prev == NULL) {
                 list = cur->next;
             } else {
                 prev->next = cur->next;
             }
 
-            free(cur->value);
-            cur->value = NULL;
-            cur->value_len = 0;
+            free(cur->key);
+            cur->key = NULL;
+            cur->key_len = 0;
             free(cur);
 
             return list;
@@ -82,9 +100,32 @@ void flb_list_free(flb_list_node* list) {
         prev = list;
         list = list->next;
 
+        free(prev->key);
+        prev->key = NULL;
+        prev->key_len = 0;
+
         free(prev->value);
         prev->value = NULL;
-        prev->value_len = 0;
+
         free(prev);
     }
+}
+
+int flb_list_foreach3(flb_list_node* list, int (*fun)(void*, const char*, const char*),
+                      void* first_arg) {
+    if (!list || !fun) {
+        return 1;
+    }
+
+    int rc = 0;
+    flb_list_node* prev = NULL;
+
+    while (list) {
+        prev = list;
+        list = list->next;
+
+        rc += fun(first_arg, prev->key, prev->value);
+    }
+
+    return rc;
 }
