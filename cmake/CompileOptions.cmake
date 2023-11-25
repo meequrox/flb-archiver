9c4e7154c45b56bb2e678d494db5c98092a0a442
meequrox <117393211+meequrox@users.noreply.github.com>
2023-11-26 02:54:07 +0700

    Format CMake files

--- cmake/CompileOptions.cmake
+++ cmake/CompileOptions.cmake
@@ -14,6 +14,6 @@ function(set_compile_options target_name)
   if(CLANG_TIDY_EXE)
     set_target_properties(${target_name} PROPERTIES
       CXX_CLANG_TIDY ${CLANG_TIDY_EXE}
-  )
+    )
   endif()
 endfunction()
